195315e994ab9e9fe82d2a31c30859885b633095
GHHag <gustav.hagenblad@gmail.com>
2022-09-30 15:37:27 +0200

    fetch test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
+
+  const [routes, setRoutes] = useState([]);
+
+  useEffect(() => {
+    getRoutesData();
+  }, []);
+
+  const getRoutesData = async () => {
+    const response = await (await fetch(`/api/searchJourney/stations/1`)).json();
+    console.log(response.data);
+    response.data.map(x => console.log(x.StartDest));
+    setRoutes(response.data);
+  }
+
   return (
-    <div>HomePage</div>
+    <main>
+      <div>HomePage</div>
+      <div>
+        {
+          routes.map((x, id) => (
+            <div key={id}>
+              {x.stationName}
+              {x.departure}
+              {x.arrival}
+            </div>
+          ))
+        }
+      </div>
+    </main>
   )
 }
 
