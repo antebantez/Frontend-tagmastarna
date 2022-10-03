53f4e5978ca297fbbf91ef75f54b9d0b03493a26
Nehcnud <nenaddjukin@gmail.com>
2022-10-03 15:10:52 +0200

    styled front page

--- src/components/SearchJourneyButton.jsx
+++ src/components/SearchJourneyButton.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
 const SearchJourney = () => {
   return (
     <Row>
-        <div id='hpDiv' className='text-center' >
-          
-        <Col className=''>
-          <Link to="/ticketPage"><Button id='testKnapp' className='border border-2 border-dark mt-5'> Sök resa</Button></Link>
-          
-          </Col>
-          </div>
-        </Row>
-  )
-}
+      <div id="hpDiv" className="text-center">
+        <Col className="">
+          <Link to="/SearchJourney">
+            <Button id="testKnapp" className="border-0 mt-5">
+              {" "}
+              Sök resa
+            </Button>
+          </Link>
+        </Col>
+      </div>
+    </Row>
+  );
+};
 
-export default SearchJourney
+export default SearchJourney;
